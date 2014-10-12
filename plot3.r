5f4a4b726532b228c448290c0891bd34e4a55b1e
klausEbehr <erichm750@gmail.com>
2014-10-12 15:08:52 -0700

    fixed some issues

--- plot3.r
+++ plot3.r
@@ -36,3 +36,7 @@ lines(x=df$datetime,  y=df$Sub_metering_3, col="Blue")
 
 legend("topright", col=c("Black", "Red", "Blue"), lty=1, lwd=2,
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Saving Plot 3 to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
